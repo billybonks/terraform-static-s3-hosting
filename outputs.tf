e2fa4a9804a1de10e34cbd59aab7c9df7129f7e4
billybonks <sebastienstettler@gmail.com>
2019-06-25 23:12:05 +0800

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,6 @@ output "host_bucket" {
   value = "${aws_s3_bucket.frontend.bucket}"
 }
 
-output "distribution_id" {
-  value = "${aws_cloudfront_distribution.frontend.id}"
-}
-
-output "distribution_domain_name" {
-  value = "${aws_cloudfront_distribution.frontend.domain_name}"
-}
-
-output "distribution_zone_id" {
-  value = "${aws_cloudfront_distribution.frontend.hosted_zone_id}"
+output "host_bucket_regional_domain_name" {
+  value = "${aws_s3_bucket.frontend.bucket_regional_domain_name}"
 }
